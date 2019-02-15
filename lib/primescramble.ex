8d4ccea2b9c0fd002f0847a6a1c429324bd85f54
pedromcorreia <pedmcor@gmail.com>
2019-02-15 16:10:33 -0200

    Add the findm index in list

--- lib/primescramble.ex
+++ lib/primescramble.ex
@@ -12,14 +12,22 @@ defmodule Primescramble do
       :world
 
   """
-  @alphabet~w(A B C D E F G H I J K L M N O P Q R S T U V W X Y Z)
+  @alphabet%{"A" => 1, "B" => 2, "C" => 3, "D" => 4, "E" => 5, "F" => 6, "G" => 7, "H" => 8, "I" => 9, "J" => 10, "K" => 11, "L" => 12, "M" => 13, "N" => 14, "O" => 15, "P" => 16, "Q" => 17, "R" => 18, "S" => 19, "T" => 20, "U" => 21, "V" => 22, "W" => 23, "X" => 24, "Y" => 25, "Z" => 26}
   def decoding(encrypted) do
     encrypted
     |> String.graphemes()
     |> Enum.map(fn x ->
       <<v::utf8>> = x
-      get_primes(v) |> List.last()
+      value_ascii =
+        find_index_alphabet(x)
+        |> Kernel.+(1)
+        |> get_primes()
+        |> List.last()
     end)
+  end
+
+  defp find_index_alphabet(word) do
+    Map.get(@alphabet, String.upcase(word))
   end
 
   def get_primes(n) when n < 2, do: []
